5bf0c0b842c924c063f6fc5dce00b0c115a95912
graci-loureiro <gracibl2017@gmail.com>
2021-02-24 12:10:55 +0000

    project second changes

---
+++ project-one-briefs/connect4.js
@@ -0,0 +1,39 @@
+//HTML
+    //grid 7 x 6 or/and the board
+
+    //circle pieces/tokens
+
+    //winner: a line of four
+      //horizontal
+      //vertical
+      //diagonals
+
+    //loser: game filled without a line of four
+
+//CSS
+    //divs, classes, id tags
+    //flexbox
+
+
+//JS
+
+  //VARIABLES
+  //ELEMENTS
+    const token
+    const winner
+    const looser
+    const filledCircle
+    const emptyCircle
+    const lineOfFour
+
+    .querySelector
+
+
+
+  //EXECUTIONS/FUNCTIONS
+    if/else condition: winner or looser 
+    .addEventListner
+    setInterval()
+
+
+  //EVENTS
---
+++ project-one-briefs/snake.js
@@ -0,0 +1,34 @@
+//HTML
+    //grid
+    //snake
+    //snake's food
+    //walls
+
+
+//CSS
+    //divs, classes, id tags
+    //flexbox
+
+
+//JS
+
+  //VARIABLES
+    let foodAppears
+    let foodsDisappears
+
+  //ELEMENTS
+    const snake 
+    const snakeAte
+    const food
+
+    .querySelector
+
+  //EXECUTIONS/FUNCTIONS
+    .addEventListner
+    setInterval()
+
+
+    //snake speeds up as it east more
+  
+  //EVENTS
+
