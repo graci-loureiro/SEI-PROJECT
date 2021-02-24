3731f4d97550b71b1b3e4d499b8c085cd84bd6f3
graci-loureiro <gracibl2017@gmail.com>
2021-02-24 16:46:22 +0000

    js variables and functions

--- project-one-briefs/mastermind/mastermind.js
+++ project-one-briefs/mastermind/mastermind.js
@@ -5,8 +5,24 @@ let peg =
 
 //elements
 
-const makerArray = [red, blue, green, yellow]
+const firstArray = [red, blue, green, yellow]
 console.log('array', makerArray)
 
+const nextArray
 
-for 
+querySelector
+
+//functions
+
+function check ()
+for (let i = 0; i < 10; i++) {
+  console.log(nextArray[i])
+  if 
+  true
+
+  else false
+
+
+//events
+addEventListener
+setInterval()
